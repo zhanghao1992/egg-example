c31acd880b749df37a48c3b43bd1dea4a225ea5e
张浩 <zhanghao@ACA8016A.ipt.aol.com>
2018-10-14 22:23:13 +0800

    egg 初体验

---
+++ config/config.default.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1539430484823_692';
+
+  // add your config here
+  config.middleware = [];
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+  // 添加 news 的配置项
+  config.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+  };
+
+  return config;
+};
